3fdceca66e46df67afba2f1b909523a0841c65ef
AuDietrich <33529775+AusDietrich@users.noreply.github.com>
2021-11-13 17:50:51 -0700

    Add files via upload

---
+++ src/main/java/com/personal/pibot/PibotApplication.java
@@ -0,0 +1,13 @@
+package com.personal.pibot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PibotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PibotApplication.class, args);
+	}
+
+}
